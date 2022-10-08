91511555534f656d2747dc934c50c7f2e29f9de0
Edward Chen <106000371+edwdch@users.noreply.github.com>
2022-10-09 01:40:14 +0800

    Update Microsoft.PowerShell_profile.ps1

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -43,14 +43,48 @@ Function mambaDeactivate {
   conda deactivate 
 }
 
+function mambaRemoveEnvironment {
+  param (
+    $mambaEnvName
+  )
+  mamba remove -y --all -n $mambaEnvName
+}
+
+function mambaCreateEnvironment {
+  param (
+    $envName,
+    $packages=''
+  )
+  if ($packages -ne '') {
+    mamba create -y --name $envName $packages
+  } else {
+    mamba create --name $envName
+  }
+}
+
+function mambaCreateEnvByFile {
+  param (
+    $filename='conda.yml'
+  )
+  mamba env create -f $filename
+}
+
+function mambaExportPackagesToYml {
+  param (
+    $filename='conda.yml'
+  )
+  conda env export --no-builds | grep -v '^prefix: ' > $filename
+}
+
 function pipInstallUseTsinghua {
   param (
-    [string]$pipPackage='',
-    [string]$r='requirements.txt'
+    [string]$pipPackage = '',
+    [string]$r = 'requirements.txt'
   )
   if ($pipPackage -ne '') {
     pip install -i https://pypi.tuna.tsinghua.edu.cn/simple $pipPackage
-  } else {
+  }
+  else {
     pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r $r
   }
 }
@@ -71,17 +105,24 @@ function gitCommonCommitAndPush {
 Set-Alias -Name mi -Value mambaInstall
 Set-Alias -Name ms -Value mambaSearch
 Set-Alias -Name ml -Value mambaListPackages
-Set-Alias -Name mel -Value mambaListEnvs
+Set-Alias -Name mle -Value mambaListEnvs
 Set-Alias -Name ma -Value mambaActivate
 Set-Alias -Name mda -Value mambaDeactivate
+Set-Alias -Name mr -Value mambaRemoveEnvironment
+Set-Alias -Name mcf -Value mambaCreateEnvByFile
+Set-Alias -Name mc -Value mambaCreateEnvironment
+Set-Alias -Name mt -Value mambaExportPackagesToYml
 
 Set-Alias -Name pipi -Value pipInstallUseTsinghua
 Set-Alias -Name pipu -Value pipUninstall
 Set-Alias -Name gitcm -Value gitCommonCommitAndPush
 
-Set-Alias -Name ll -Value ls
 Set-Alias -Name g -Value git
+Set-Alias -Name vim -Value nvim
+
+Set-Alias -Name ll -Value ls
 Set-Alias -Name grep -Value findstr
+
 # === MY SELF ALIAS ===
 
 # === Oh-My-Posh ===
